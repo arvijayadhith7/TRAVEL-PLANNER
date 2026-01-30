033273431dfe2b7d2e11206e8f2399122aca207f
vijayaksith <vijayadhith.a.r@gmail.com>
2026-01-30 20:50:03 +0530

    Removed Google Auth button and corresponding logic

--- login.js
+++ login.js
@@ -1,17 +1,3 @@
-function handleCredentialResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-    const responsePayload = parseJwt(response.credential);
-    alert(`Welcome back, ${responsePayload.name}! Accessing Voyanix Mobile...`);
-}
-
-function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
-};
 
 document.addEventListener('DOMContentLoaded', () => {
     const authForm = document.getElementById('authForm');
